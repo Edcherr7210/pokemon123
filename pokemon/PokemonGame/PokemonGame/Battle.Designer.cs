b89d4be936c59d915718ba2b31f55e34cc540e02
Edcherr7210 <dadocherritte@gmail.com>
2025-04-15 16:50:09 -0400

    Add files via upload

---
+++ pokemon/PokemonGame/PokemonGame/Battle.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace PokemonGame
+{
+    partial class Battle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary> 
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+
+        private void InitializeComponent()
+        {
+            team1Poke = new PictureBox();
+            team2Poke = new PictureBox();
+            progressBar1 = new ProgressBar();
+            progressBar2 = new ProgressBar();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            team2Poke2 = new PictureBox();
+            team2Poke3 = new PictureBox();
+            team2Poke5 = new PictureBox();
+            team2Poke6 = new PictureBox();
+            team2Poke4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)team1Poke).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke4).BeginInit();
+            SuspendLayout();
+            // 
+            // team1Poke
+            // 
+            team1Poke.BackgroundImageLayout = ImageLayout.Zoom;
+            team1Poke.Location = new Point(72, 208);
+            team1Poke.Name = "team1Poke";
+            team1Poke.Size = new Size(230, 165);
+            team1Poke.TabIndex = 0;
+            team1Poke.TabStop = false;
+            // 
+            // team2Poke
+            // 
+            team2Poke.BackgroundImageLayout = ImageLayout.Zoom;
+            team2Poke.Location = new Point(636, 96);
+            team2Poke.Name = "team2Poke";
+            team2Poke.Size = new Size(230, 165);
+            team2Poke.TabIndex = 1;
+            team2Poke.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(636, 35);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(228, 37);
+            progressBar1.TabIndex = 2;
+            // 
+            // progressBar2
+            // 
+            progressBar2.Location = new Point(78, 145);
+            progressBar2.Name = "progressBar2";
+            progressBar2.Size = new Size(228, 37);
+            progressBar2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(2, 410);
+            button1.Name = "button1";
+            button1.Size = new Size(234, 131);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(235, 410);
+            button2.Name = "button2";
+            button2.Size = new Size(237, 131);
+            button2.TabIndex = 6;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(452, 410);
+            button3.Name = "button3";
+            button3.Size = new Size(238, 131);
+            button3.TabIndex = 7;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(681, 410);
+            button4.Name = "button4";
+            button4.Size = new Size(238, 131);
+            button4.TabIndex = 8;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // team2Poke2
+            // 
+            team2Poke2.BackgroundImageLayout = ImageLayout.Zoom;
+            team2Poke2.Location = new Point(505, 30);
+            team2Poke2.Name = "team2Poke2";
+            team2Poke2.Size = new Size(120, 71);
+            team2Poke2.TabIndex = 9;
+            team2Poke2.TabStop = false;
+            // 
+            // team2Poke3
+            // 
+            team2Poke3.BackgroundImageLayout = ImageLayout.Zoom;
+            team2Poke3.Location = new Point(379, 30);
+            team2Poke3.Name = "team2Poke3";
+            team2Poke3.Size = new Size(120, 71);
+            team2Poke3.TabIndex = 10;
+            team2Poke3.TabStop = false;
+            // 
+            // team2Poke5
+            // 
+            team2Poke5.BackgroundImageLayout = ImageLayout.Zoom;
+            team2Poke5.Location = new Point(124, 30);
+            team2Poke5.Name = "team2Poke5";
+            team2Poke5.Size = new Size(120, 71);
+            team2Poke5.TabIndex = 11;
+            team2Poke5.TabStop = false;
+            // 
+            // team2Poke6
+            // 
+            team2Poke6.BackgroundImageLayout = ImageLayout.Zoom;
+            team2Poke6.Location = new Point(-2, 30);
+            team2Poke6.Name = "team2Poke6";
+            team2Poke6.Size = new Size(120, 71);
+            team2Poke6.TabIndex = 12;
+            team2Poke6.TabStop = false;
+            // 
+            // team2Poke4
+            // 
+            team2Poke4.BackgroundImageLayout = ImageLayout.Zoom;
+            team2Poke4.Location = new Point(250, 30);
+            team2Poke4.Name = "team2Poke4";
+            team2Poke4.Size = new Size(120, 71);
+            team2Poke4.TabIndex = 13;
+            team2Poke4.TabStop = false;
+            // 
+            // Battle
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(967, 583);
+            Controls.Add(team2Poke4);
+            Controls.Add(team2Poke6);
+            Controls.Add(team2Poke5);
+            Controls.Add(team2Poke3);
+            Controls.Add(team2Poke2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(progressBar2);
+            Controls.Add(progressBar1);
+            Controls.Add(team2Poke);
+            Controls.Add(team1Poke);
+            Name = "Battle";
+            Text = "Battle";
+            ((System.ComponentModel.ISupportInitialize)team1Poke).EndInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke).EndInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)team2Poke4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox team1Poke;
+        private PictureBox team2Poke;
+        private ProgressBar progressBar1;
+        private ProgressBar progressBar2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private PictureBox team2Poke2;
+        private PictureBox team2Poke3;
+        private PictureBox team2Poke5;
+        private PictureBox team2Poke6;
+        private PictureBox team2Poke4;
+    }
+}
